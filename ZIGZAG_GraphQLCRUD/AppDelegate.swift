3bb6abbb1df3f05194b46811244e393d40a3e9c2
Paul Kim <parahop94@gmail.com>
2019-10-15 06:22:43 +0900

    bugfix: device orientation animation will not work without these boiierplate codes

--- ZIGZAG_GraphQLCRUD/AppDelegate.swift
+++ ZIGZAG_GraphQLCRUD/AppDelegate.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder {
     let networkManager = ZAPIManager()
 }
 
+extension AppDelegate: UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
